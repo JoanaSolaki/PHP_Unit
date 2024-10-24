e7fbc9fd0ae568d390f276c1e6b290b0c9b3dae9
Joana <joana.rodrigues.simplon@gmail.com>
2024-10-24 11:47:49 +0200

    Pipeline test v5

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegistrationType;
+use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,17 +14,21 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomepageController extends AbstractController
 {
     #[Route('/', name: 'app_homepage', methods: ['GET', 'POST'])]
-    public function index(EntityManagerInterface $entityManager, Request $request): Response {
+    public function index(EntityManagerInterface $entityManager, Request $request, UserRepository $userRepository): Response {
         $user = new User();
         $form = $this->createForm(RegistrationType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($user);
-            $entityManager->flush();
-
-            $this->addFlash('success', 'Votre inscription a bien été prise en compte !');
-            return $this->redirectToRoute('app_homepage');
+            $existingUser = $userRepository->findOneBy(['email' => $user->getEmail()]);
+            if ($existingUser) {
+                $this->addFlash('error', 'Cet email est déjà utilisé pour une inscription.');
+            } else {
+                $entityManager->persist($user);
+                $entityManager->flush();
+                $this->addFlash('success', 'Votre inscription a bien été prise en compte !');
+                return $this->redirectToRoute('app_homepage');
+            }
         }
 
         return $this->render('homepage.html.twig', [
