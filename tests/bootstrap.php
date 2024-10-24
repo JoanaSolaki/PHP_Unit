d2578b5d7131741710429639d315c841986c4269
Joana <joana.rodrigues.simplon@gmail.com>
2024-10-24 12:44:18 +0200

    Pipeline test v12

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,9 +7,9 @@ require dirname(__DIR__) . '/vendor/autoload.php';
 if (file_exists(dirname(__DIR__) . '/config/bootstrap.php')) {
     require dirname(__DIR__) . '/config/bootstrap.php';
 } else {
-    if (getenv('CI')) {
-        (new Dotenv())->bootEnv(dirname(__DIR__) . '/.env.test');
-    } elseif (file_exists(dirname(__DIR__) . '/.env.test.local')) {
+    if (file_exists(dirname(__DIR__) . '/.env.test.local')) {
         (new Dotenv())->bootEnv(dirname(__DIR__) . '/.env.test.local');
-    }
+    } else if (file_exists(dirname(__DIR__) . '/.env.test')) {
+        (new Dotenv())->bootEnv(dirname(__DIR__) . '/.env.test');
+    }    
 }
