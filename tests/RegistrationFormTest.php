70425ef077f3abfc8bc7805ca28ac99c14770b1a
Joana <joana.rodrigues.simplon@gmail.com>
2024-10-24 16:11:10 +0200

    Test Mail Exist already

--- tests/RegistrationFormTest.php
+++ tests/RegistrationFormTest.php
@@ -23,4 +23,21 @@ class RegistrationFormTest extends WebTestCase {
         $client->followRedirect();
         $this->assertSelectorTextContains('.alert-success', 'Votre inscription a bien été prise en compte !');
     }
+
+    public function testFormSubmissionWithExistingEmail() {
+        $client = static::createClient();
+        
+        $crawler = $client->request('GET', '/');
+        $this->assertResponseIsSuccessful();
+
+        $form = $crawler->selectButton("S'inscrire")->form([
+            'registration[Name]' => 'Stark',
+            'registration[Surname]' => 'Tony',
+            'registration[email]' => 'tony.stark@avengers.com',
+        ]);
+
+        $client->submit($form);
+
+        $this->assertSelectorTextContains('.alert-error', 'Cet email est déjà utilisé pour une inscription.');
+    }
 }
